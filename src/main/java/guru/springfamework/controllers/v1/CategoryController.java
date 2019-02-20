0c50e3bb7438b32a76857516c40f79e4985f4919
markopaco <marko_stankovic@web.de>
2019-02-20 14:44:24 +0100

    Add and test some swagger annotations.

--- src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -3,9 +3,12 @@ package guru.springfamework.controllers.v1;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.api.v1.model.CategoryListDTO;
 import guru.springfamework.services.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "This is my Category Controller")
 @RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
@@ -27,6 +30,7 @@ public class CategoryController {
     }*/
 
     //With RestController annotation we automatically return a ResponseBody
+    @ApiOperation(value = "This will get a list of categories.", notes = "This are some notes about the API.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CategoryListDTO getAllCategories() {
